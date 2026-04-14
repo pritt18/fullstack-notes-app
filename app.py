0cee1a0ea21bba2f3f2ff7829a236f6749e426f6
pritt18 <pritamgangurde18@gmail.com>
2026-04-14 16:27:38 +0530

    Fix deployment error

--- app.py
+++ app.py
@@ -117,7 +117,9 @@ def logout():
 
 # ------------------ RUN ------------------
 
+import os
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
